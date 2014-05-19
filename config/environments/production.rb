82cd52000fd458b56227131ac1b0ada097dfd27e
charlyy <cyoon90@yahoo.com>
2014-05-19 03:25:52 -0700

    Push to heroku pleasegit status

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  # config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -85,5 +85,4 @@ Rails.application.configure do
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
-}
-end
+}end
